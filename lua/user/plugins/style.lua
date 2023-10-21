6283210170d5dde261a6daf5093e8a2f9f4ae846
Garry Filakhtov <filakhtov@gmail.com>
2023-10-21 20:49:18 +1100

    set night theme, add line numbers, current line highlight

--- lua/user/plugins/style.lua
+++ lua/user/plugins/style.lua
@@ -1,8 +1,20 @@
+vim.wo.number = true
+vim.o.termguicolors = true
+vim.o.breakindent = true
+vim.o.showbreak = '󱞩 '
+vim.o.cursorline = true
+
+local function setupTheme()
+  vim.cmd.colorscheme('tokyonight-night')
+
+  local colors = require('tokyonight.colors')
+
+  vim.api.nvim_set_hl(0, 'CursorLineNr', { bold = true, fg = colors.default.dark5 })
+  vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.default.dark3 })
+end
+
 return {
   'folke/tokyonight.nvim',
   tag = 'v2.8.0',
-  config = function()
-    vim.opt.termguicolors = true
-    vim.cmd.colorscheme('tokyonight')
-  end,
+  config = setupTheme,
 }
