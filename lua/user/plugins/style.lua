78e43ccf51fac8bd23c66dc5ade1e19cde6c4fe7
Garry Filakhtov <filakhtov@gmail.com>
2023-10-21 21:25:52 +1100

    better way to handle color changes

--- lua/user/plugins/style.lua
+++ lua/user/plugins/style.lua
@@ -5,12 +5,14 @@ vim.o.showbreak = '󱞩 '
 vim.o.cursorline = true
 
 local function setupTheme()
+  require('tokyonight').setup({
+    on_highlights = function(highlights, colors)
+      highlights.CursorLineNr = { fg = colors.dark5, bold = true }
+      highlights.LineNr = { fg = colors.dark3 }
+    end
+  })
+
   vim.cmd.colorscheme('tokyonight-night')
-
-  local colors = require('tokyonight.colors')
-
-  vim.api.nvim_set_hl(0, 'CursorLineNr', { bold = true, fg = colors.default.dark5 })
-  vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.default.dark3 })
 end
 
 return {
