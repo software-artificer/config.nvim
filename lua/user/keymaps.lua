dd27ae37736fb7382aa9c84de6042da40cc6d4ec
Garry Filakhtov <filakhtov@gmail.com>
2023-11-02 13:23:45 +1100

    more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,2 +1,5 @@
 vim.keymap.set('n', '<leader>q', vim.cmd.bwipeout)
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>F', vim.lsp.buf.format)
