e7881faa542460bd9c1f72faf5ab0afaaa0ee09f
Garry Filakhtov <filakhtov@gmail.com>
2023-11-02 13:09:11 +1100

    add more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '<leader>q', vim.cmd.bwipeout)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
--- lua/user/plugins/lsp_signature.lua
+++ lua/user/plugins/lsp_signature.lua
@@ -3,6 +3,7 @@ local function setupPlugin()
     floating_window = false,
     doc_lines = 0,
     hint_prefix = '󱃺 ',
+    --select_signature_key = '' -- switch between multiple signatures for the same function name
   })
 end
 
