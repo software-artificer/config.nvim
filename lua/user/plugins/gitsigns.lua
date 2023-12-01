a01b827d62c731be93d88a10769156b9071ffddd
Garry Filakhtov <garry.filakhtov@bigcommerce.com>
2023-12-01 17:32:37 +1100

    GitSigns: refactor the way keymaps are assigned

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -9,67 +9,67 @@ return {
     on_attach = function(bufnr)
       local gitsigns = package.loaded.gitsigns
 
-      keymap_set(
+      local function map(modes, keymaps, action, description)
+        keymap_set(
+          modes,
+          keymaps,
+          action,
+          { buffer = bufnr, desc = description }
+        )
+      end
+
+      map(
         { 'n', 'v' },
         '<leader>gb',
         gitsigns.toggle_current_line_blame,
-        { buffer = bufnr, desc = '󰋇 GitSigns: Enable current line blame' }
+        '󰋇 GitSigns: Enable current line blame'
       )
-      keymap_set('n', '<leader>gB', function()
+      map('n', '<leader>gB', function()
         gitsigns.blame_line({ full = true })
-      end, {
-        buffer = bufnr,
-        desc = '󰋇 GitSigns: Show blame for line under the cursor',
-      })
-      keymap_set(
+      end, '󰋇 GitSigns: Show blame for line under the cursor')
+      map(
         'n',
         '<leader>gp',
         gitsigns.preview_hunk_inline,
-        { buffer = bufnr, desc = ' GitSigns: Preview hunk inline' }
+        ' GitSigns: Preview hunk inline'
       )
-      keymap_set(
+      map(
         'n',
         '<leader>gP',
         gitsigns.preview_hunk,
-        { buffer = bufnr, desc = ' GitSigns: Preview hunk' }
+        ' GitSigns: Preview hunk'
       )
-      keymap_set(
+      map(
         'n',
         '<leader>gd',
         gitsigns.toggle_deleted,
-        { buffer = bufnr, desc = '󱀸 GitSigns: Toggle deleted lines' }
+        '󱀸 GitSigns: Toggle deleted lines'
       )
-      keymap_set(
-        'n',
-        '<leader>gs',
-        gitsigns.stage_hunk,
-        { buffer = bufnr, desc = ' GitSigns: Stage hunk' }
-      )
-      keymap_set('v', '<leader>gs', function()
+      map('n', '<leader>gs', gitsigns.stage_hunk, ' GitSigns: Stage hunk')
+      map('v', '<leader>gs', function()
         gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end, { buffer = bufnr, desc = '󰐒 GitSigns: Stage selection' })
-      keymap_set(
-        'n',
-        '<leader>gr',
-        gitsigns.reset_hunk,
-        { buffer = bufnr, desc = ' GitSigns: Reset hunk' }
-      )
-      keymap_set('v', '<leader>gr', function()
+      end, '󰐒 GitSigns: Stage selection')
+      map('n', '<leader>gr', gitsigns.reset_hunk, ' GitSigns: Reset hunk')
+      map('v', '<leader>gr', function()
         gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end, { buffer = bufnr, desc = '󰐓 GitSigns: Reset selection' })
-      keymap_set({ 'n', 'v' }, '<leader>gS', gitsigns.stage_buffer, {
-        buffer = bufnr,
-        desc = '󱪝 GitSigns: Stage all hunks in the buffer',
-      })
-      keymap_set({ 'n', 'v' }, '<leader>gR', gitsigns.reset_buffer, {
-        buffer = bufnr,
-        desc = '󱪟 GitSigns: Reset all hunks in the buffer',
-      })
-      keymap_set(
+      end, '󰐓 GitSigns: Reset selection')
+      map(
+        { 'n', 'v' },
+        '<leader>gS',
+        gitsigns.stage_buffer,
+        '󱪝 GitSigns: Stage all hunks in the buffer'
+      )
+      map(
+        { 'n', 'v' },
+        '<leader>gR',
+        gitsigns.reset_buffer,
+        '󱪟 GitSigns: Reset all hunks in the buffer'
+      )
+      map(
         { 'n', 'v' },
         '<leader>gu',
         gitsigns.undo_stage_hunk,
-        { buffer = bufnr, desc = '󰤘 GitSigns: Undo staged hunk' }
+        '󰤘 GitSigns: Undo staged hunk'
       )
     end,
   },
