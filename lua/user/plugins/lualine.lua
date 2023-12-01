caf86c120fc9df11cc050da45c69e075afede7ab
Garry Filakhtov <filakhtov@gmail.com>
2023-12-01 19:44:57 +1100

    lualine: configure winbar style

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -2,10 +2,41 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
+    'SmiteshP/nvim-navic',
   },
   opts = {
     options = {
       theme = 'codedark',
+    },
+    sections = {
+      lualine_b = {
+        'branch',
+        {
+          'diff',
+          symbols = {
+            added = ' ',
+            modified = ' ',
+            removed = ' ',
+          },
+        },
+        'diagnostics',
+      },
+      lualine_c = {},
+    },
+    winbar = {
+      lualine_b = {
+        'filename',
+      },
+      lualine_c = {
+        {
+          function()
+            return require('nvim-navic').get_location()
+          end,
+          cond = function()
+            return require('nvim-navic').is_available()
+          end,
+        },
+      },
     },
   },
 }
