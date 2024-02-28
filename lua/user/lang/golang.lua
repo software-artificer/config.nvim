d6eb1214a4d96c003c5bb73496101160d9d8ff42
Garry Filakhtov <garry.filakhtov@bigcommerce.com>
2024-02-28 23:03:49 +1100

    lang:golang: introduce golang LSP and DAP support

---
+++ lua/user/lang/golang.lua
@@ -0,0 +1,46 @@
+local function setupGoLsp()
+  require('lspconfig').gopls.setup({}, {
+    settings = {
+      ['gopls'] = {},
+    },
+  })
+end
+
+local function setupGoDap()
+  local dap_go = require('dap-go')
+
+  dap_go.setup({
+    delve = {
+      path = 'dlv',
+      initialize_timeout_sec = 20,
+      port = '${port}',
+    },
+  })
+
+  keymap_set('n', '<leader>Dt', function()
+    dap_go.debug_test()
+  end, { desc = '󰨰 DAP: go - debug the test' })
+  keymap_set('n', '<leader>DT', function()
+    dap_go.debug_last_test()
+  end, { desc = '󰨰 DAP: go - debug last test' })
+end
+
+return {
+  {
+    name = 'lang:go:lsp',
+    depends = { 'lang:common', 'neovim/nvim-lspconfig' },
+    dir = '.',
+    config = setupGoLsp,
+    cond = function()
+      return vim.fn.executable('gopls') == 1
+    end,
+  },
+  {
+    'leoluz/nvim-dap-go',
+    depends = { 'lang:common', 'mfussenegger/nvim-dap' },
+    config = setupGoDap,
+    cond = function()
+      return vim.fn.executable('dlv') == 1
+    end,
+  },
+}
