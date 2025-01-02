6533af49a016ea5f784593b568ea9c53763c3cc0
Garry Filakhtov <garry.filakhtov@bigcommerce.com>
2025-01-02 13:11:01 +1100

    lang:html: Enable HTML LSP

---
+++ lua/user/lang/html.lua
@@ -0,0 +1,23 @@
+local has_lsp = vim.fn.executable('vscode-html-language-server') == 1
+
+local function setupLsp()
+  if not has_lsp then
+    return
+  end
+
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  require('lspconfig').html.setup({
+    capabilities = capabilities,
+  })
+end
+
+return {
+  dependencies = function()
+    return {}
+  end,
+  setup = function()
+    setupLsp()
+  end,
+}
