a5baf5cab0ff085a1138785cde5edc872d386892
Garry Filakhtov <garry@hachiko.energy>
2025-02-21 17:33:12 +1100

    lang:typescript: Support for ESLint

--- lua/user/lang/typescript.lua
+++ lua/user/lang/typescript.lua
@@ -1,4 +1,5 @@
 local has_lsp = vim.fn.executable('typescript-language-server') == 1
+local has_eslint = vim.fn.executable('vscode-eslint-language-server') == 1
 
 local function setupLsp()
   if not has_lsp then
@@ -8,8 +9,17 @@ local function setupLsp()
   require('lspconfig').ts_ls.setup({})
 end
 
+local function setupEslint()
+  if not has_eslint then
+    return
+  end
+
+  require('lspconfig').eslint.setup({})
+end
+
 return {
   setup = function()
     setupLsp()
+    setupEslint()
   end,
 }
