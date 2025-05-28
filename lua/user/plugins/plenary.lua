a069c820fab72cfdf8fa460b5f63d0db5fca9dfc
Garry Filakhtov <filakhtov@gmail.com>
2025-05-28 19:10:04 +1000

    NeoVim 0.11: Start configuration from scratch

--- lua/user/plugins/plenary.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'nvim-lua/plenary.nvim',
-  version = '^0.1.4',
-}
--- lua/user/plugins/virt-column.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'lukas-reineke/virt-column.nvim',
-  version = '^2.0.3',
-  opts = {
-    virtcolumn = '81',
-    highlight = 'VirtColumn',
-    char = '│',
-  },
-}
