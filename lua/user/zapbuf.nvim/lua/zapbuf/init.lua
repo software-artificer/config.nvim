ec5e3793cdc5e3888c9ddeafbb6ff42ba4c1d4ba
Garry Filakhtov <filakhtov@gmail.com>
2025-06-15 19:39:27 +1000

    zapbuf: custom plugin to close windows/tabs/buffers

---
+++ lua/user/zapbuf.nvim/lua/zapbuf/init.lua
@@ -0,0 +1,131 @@
+local M = {}
+
+function M.setup(opts) end
+
+local function get_buf_name(buf_id)
+  local name = vim.api.nvim_buf_get_name(buf_id)
+
+  if name ~= '' then
+    return name
+  end
+
+  return '[No Name]'
+end
+
+local Action = {
+  WRITE = 1,
+  DISCARD = 2,
+  ABORT = 3,
+}
+
+local function handle_buf_modified(buf_id)
+  if vim.api.nvim_get_option_value('modified', { buf = buf_id }) then
+    local prompt = string.format(
+      'Buffer (%d) "%s" is modified. Do you want to save it?',
+      buf_id,
+      get_buf_name(buf_id)
+    )
+
+    local choice = vim.fn.confirm(prompt, '&Yes\n&No\n&Cancel', '3', 'Q')
+
+    if choice == 1 then
+      return Action.WRITE
+    end
+
+    if choice == 2 then
+      return Action.DISCARD
+    end
+
+    return Action.ABORT
+  end
+
+  return Action.DISCARD
+end
+
+local function close_tab()
+    local win_ids = vim.api.nvim_tabpage_list_wins(0)
+
+    for _, win_id in next, win_ids do
+      local buf_id = vim.api.nvim_win_get_buf(win_id)
+      local buf_wins = vim.fn.win_findbuf(buf_id)
+
+      if #buf_wins == 1 then
+        local action = handle_buf_modified(buf_id)
+
+        if action == Action.ABORT then
+          return
+        end
+
+        local force = false
+        if action == Action.DISCARD then
+          force = true
+        end
+
+        if action == Action.WRITE then
+          vim.api.nvim_buf_call(buf_id, vim.cmd.write)
+        end
+
+        vim.api.nvim_buf_delete(buf_id, { force = force })
+      else
+        vim.api.nvim_win_close(win_id, {})
+      end
+    end
+end
+
+function M.zap()
+  local tabs = vim.api.nvim_list_tabpages()
+  if #tabs > 1 then
+    close_tab()
+  end
+
+  local curr_win_id = vim.api.nvim_get_current_win()
+  local curr_win_conf = vim.api.nvim_win_get_config(curr_win_id)
+  if curr_win_conf.relative ~= '' then
+    vim.api.nvim_win_close(curr_win_id, {})
+  end
+
+  local curr_buf_id = vim.api.nvim_get_current_buf()
+  local curr_buf_wins = vim.fn.win_findbuf(curr_buf_id)
+
+  local force = false
+  if #curr_buf_wins == 1 then
+    local action = handle_buf_modified(curr_buf_id)
+
+    if action == Action.ABORT then
+      return
+    end
+
+    if action == Action.WRITE then
+      vim.api.nvim_buf_call(curr_buf_id, vim.cmd.write)
+    end
+
+    if action == Action.DISCARD then
+      force = true
+    end
+  end
+
+  local buf_ids = vim.api.nvim_list_bufs()
+  local loaded_buf_ids = vim.tbl_filter(function(buf_id)
+    return vim.api.nvim_buf_is_loaded(buf_id)
+  end, buf_ids)
+  local next_buf_id = nil
+  for _, buf_id in next, loaded_buf_ids do
+    local buf_wins = vim.fn.win_findbuf(buf_id)
+    if buf_id ~= curr_buf_id and #buf_wins == 0 then
+      next_buf_id = buf_id
+      break
+    end
+  end
+
+  if next_buf_id == nil then
+    next_buf_id = vim.api.nvim_create_buf(false, false)
+  end
+
+  vim.api.nvim_win_set_buf(curr_win_id, next_buf_id)
+
+  if #curr_buf_wins == 1 then
+    vim.api.nvim_buf_delete(curr_buf_id, { force = force })
+  end
+end
+
+return M
