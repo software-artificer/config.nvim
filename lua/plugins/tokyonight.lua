e15973a548eb87dc0fafeae8668fb5736186a043
Garry Filakhtov <filakhtov@gmail.com>
2025-06-17 22:44:36 +1000

    Install tokyonight theme

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,19 @@
+return {
+  'folke/tokyonight.nvim',
+  version = '^4',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    on_colors = function(colors) end,
+    on_highlights = function(highlights, colors)
+      highlights.CursorLineNr = { fg = colors.fg_dark, bold = true, bg = colors.bg_highlight }
+      highlights.LineNr = { fg = colors.dark3 }
+    end,
+    style = 'moon',
+  },
+  config = function(_, opts)
+    require('tokyonight').setup(opts)
+
+    vim.cmd.colorscheme('tokyonight')
+  end
+}
