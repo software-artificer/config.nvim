d0ec42f9a98b6a57da8e9707586b3c2ebf282140
Garry Filakhtov <filakhtov@gmail.com>
2025-07-04 18:52:23 +1000

    lsp: enable inlay hints if server supports them

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -12,3 +12,13 @@ vim.diagnostic.config({
     },
   },
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+      end
+  end,
+})
