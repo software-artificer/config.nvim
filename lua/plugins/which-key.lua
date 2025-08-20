a379844f132d2d5b04e8927dc90184bbb6f1242a
Garry Filakhtov <filakhtov@gmail.com>
2025-08-20 23:19:05 +1000

    which-key: fix formatting

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -36,35 +36,45 @@ return {
       },
       {
         'gd',
-        function() vim.lsp.buf.definition() end,
+        function()
+          vim.lsp.buf.definition()
+        end,
         mode = 'n',
         desc = 'Go to definition',
         icon = { icon = '', color = 'orange' },
       },
       {
         'gD',
-        function() vim.lsp.buf.declaration() end,
+        function()
+          vim.lsp.buf.declaration()
+        end,
         mode = 'n',
         desc = 'Go to declaration',
         icon = { icon = '', color = 'orange' },
       },
       {
         '<leader>fS',
-        function() vim.lsp.buf.workspace_symbol() end,
+        function()
+          vim.lsp.buf.workspace_symbol()
+        end,
         mode = 'n',
         desc = '[F]ind workspace [s]ymbols',
         icon = { icon = '󱊓', color = 'orange' },
       },
       {
         '<leader>lcr',
-        function() vim.lsp.codelens.run() end,
+        function()
+          vim.lsp.codelens.run()
+        end,
         mode = 'n',
         desc = 'Run the [L]SP [c]odelens',
         icon = { icon = '󱖪', color = 'orange' },
       },
       {
         '<leader>lf',
-        function() vim.lsp.buf.format() end,
+        function()
+          vim.lsp.buf.format()
+        end,
         mode = 'n',
         desc = '[L]SP [f]ormat the document',
         icon = { icon = '', color = 'orange' },
