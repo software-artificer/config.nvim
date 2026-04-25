9a87b74834ce4b7e6f63f4748fdef65450587027
Garry Filakhtov <garry@hachiko.energy>
2026-04-25 23:21:26 +1000

    json: add jsonls language server

---
+++ lua/lang/json.lua
@@ -0,0 +1,10 @@
+local have_jsonls = vim.fn.executable('vscode-json-language-server') == 1
+
+return {
+  name = 'lang:json',
+  cond = have_jsonls,
+  dir = vim.fn.stdpath('config') .. '/lua/user/lang/json/',
+  config = function(_, opts)
+    vim.lsp.enable('jsonls')
+  end,
+}
